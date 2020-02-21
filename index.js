cbba1c4c1df234adb4afbc3b253f722351684cab
Jacob Reilly-Cooper <jacobreilly-cooper@Jacobs-MBP.lan>
2020-02-21 23:50:51 +0000

    Harry Potter API updated. Written data to json file for future use. Tested with individual characters. More to come with the data.

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 const request = require('request');
 const hbs = require('express-handlebars');
+const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const fs = require('fs');
 
 require('dotenv').config()
 
-const getHarryPotterData = () => {
-    request({
-            uri: `https://www.potterapi.com/v1/characters?key=${process.env.key}`,
-            json: true
-        }, (err, res) => {
-            if (err) throw err;
-            console.log(res.body);
+const getHarryPotterData = require('./lib/getHarryPotter')
+const app = express();
 
-        })
-}
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-getHarryPotterData()
+app.engine('.hbs', hbs({
+    defaultLayout: 'layout',
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+app.get('/', async (req, res) => {
+    let data = await getHarryPotterData();
+    // console.log(data)
+    fs.writeFileSync('HarryPotter.json', data)
+    let name = data.name;
+    let house = data.house;
+    console.log(name);
+    console.log(house);
+    
+    
+    res.render('index');
+});
+
+app.listen(3001, () => {
+    console.log('server listening on port 3000');
+    console.log(__dirname);
+});
