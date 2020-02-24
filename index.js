d4612d57579e94f751164c3c36fe53c69df61184
Jacob Reilly-Cooper <jacobreilly-cooper@Jacobs-MacBook-Pro.local>
2020-02-24 12:48:17 +0000

    Harry Potter houses now displaying beautifully on the houses page. My goodness. Cannot currently cycle through them at will, but comments in code on how this will be done are present.

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ const HarryPotterData = require('./lib/getHarryPotter')
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 app.engine('.hbs', hbs({
@@ -29,11 +31,11 @@ app.get('/', async (req, res) => {
     // let house = data[0].house;
     // console.log(name);
     // console.log(house);
-    
+
     res.render('index');
 });
 
-app.get('/characters', (req, res)=> {
+app.get('/characters', (req, res) => {
     res.render('characters')
 })
 
@@ -42,36 +44,66 @@ app.post('/characters', async (req, res) => {
 
     console.log(characterChoice)
 
-    let data = await HarryPotterData.getHarryPotterData(characterChoice)
+    let data = await HarryPotterData.getCharacterData(characterChoice)
     fs.writeFileSync('./JsonFiles/ReturnedInfo.json', data)
     console.log(data)
 
-    let name = data[0].name;
-    let house = data[0].house;
-    let role = data[0].role;
-    let wand = data[0].wand;
-    
-    // console.log(name)
-    // console.log(house)
+    if (data[0]) {
+        let name = data[0].name;
+        let house = data[0].house;
+        let role = data[0].role;
+        let wand = data[0].wand;
+        let school = data[0].school;
+
+        res.render('characters', {
+            data: {
+                name,
+                house,
+                role,
+                wand,
+                school
+            }
+        })
+        return
+    }
 
     res.render('characters', {
-        data: {
-            name,
-            house,
-            role,
-            wand
-        }
+        err: 'nothing found'
     })
 })
 
-app.get('/houses', (req, res)=> {
+app.get('/houses', async (req, res) => {
+
     res.render('houses')
 })
 
-// app.post('/houses', (req, res) => {
+app.post('/houses', async (req, res) => {
+    let data = await HarryPotterData.getHouseData();
+    // fs.writeFileSync('./JsonFiles/houseData.json', data)
 
-//     res.render('houses')
-// })
+    let houses = []
+    /*
+    
+    if item.name == Gryffindor then return the object information on Gryffindor 
+    the reason for this, to allow the user to choose which house information they'd like to display.
+
+    */
+    for (const item of data) {
+        houses.push({
+            name: item.name,
+            mascot: item.mascot,
+            headOfHouse: item.headOfHouse,
+            founder: item.founder
+        })
+    }
+
+    console.log(houses);
+
+
+    res.render('houses', {
+        houses
+    })
+})
 
 app.listen(3001, () => {
     console.log('server listening on port 3001');
